d869a9632d66bdf965dea3bbe56544e53284bba4
Noah Teshu <nteshu@gmail.com>
2015-01-12 12:38:18 -0500

    added support for displaying images

--- javascript.js
+++ javascript.js
@@ -1,9 +1,14 @@
 Parse.initialize("HLh6vGMEIITwBefYZUQ2b6K1uvrzrBEw459Qe0iE", "5Lxv4WecXr7Kt3xvA7D4Kr3l2nJ3LM9lJR2hiCUf");
 
-var Activity = function(name, creator, link){
+var Activity = function(name, creator, link, objectId, description, location, phoneNumber, image){
 this.name = name;
 this.creator = creator;
 this.link = link
+this.objectId = objectId;
+this.description = description;
+this.location = location;
+this.phoneNumber = phoneNumber;
+this.image = image;
 
 };
 //alert("hey!");
@@ -38,20 +43,35 @@ function findActivities() {
 	var activityQuery = new Parse.Query(activity);
 	activityQuery.find({
 		success: function(results){
-
+			console.log(results);
 			for (var i = 0; i < results.length; i++) {
-
-				myActivities.push(results[i].attributes);
+				var name = results[i].attributes.activityName;
+				var creator = results[i].attributes.activityCreator;
+				var link = results[i].attributes.linkField;
+				var objectId = results[i].id;
+				var description = results[i].attributes.descriptionField;
+				var location = results[i].attributes.locationField;
+				var phoneNumber = results[i].attributes.phoneNumberField;
+				var image = results[i].attributes.activityImage;
+				var activity = new Activity(name, creator, link, objectId, description, location, phoneNumber,image);
+				myActivities.push(activity);
 			};
-			console.log(myActivities);
 			for (var i = 0; i < myActivities.length; i++) {
-				$(".activityList").append("<li class='activityInList' data-activityindex='" + i+"'>"+myActivities[i].activityName+ "</li>"); 	
+				$(".activityList").append("<li class='activityInList' data-activityindex='" + i+"'>"+myActivities[i].name+ "</li>"); 	
 			};
 			$(".activityList").addClass("activitiesBackground");
 
 			$("li").click(function(){
+				if (myActivities[$(this).data("activityindex")].image !== undefined) {
+					var imagePath = myActivities[$(this).data("activityindex")].image.url();	
+				};
+				
 				$(".activityList").empty();
-				$(".activityList").append("<li class='activityInList'>"+myActivities[$(this).data("activityindex")].activityName+ "</li>").append("<button id='backButton'>Back</button>");
+				$(".activityList").append("<li class='activityInList'>"+myActivities[$(this).data("activityindex")].name+ "</li>")
+				if (imagePath !== undefined) {
+					$(".activityList").append("<img src='" + imagePath+"'> </img>");
+				};
+				$(".activityList").append("<button id='backButton'>Back</button>");
 				$("#backButton").click(function(){
 					findActivities();
 				});
